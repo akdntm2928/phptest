ccdcf5b257f19e62523eceb987bb99b6c7c07fea
swh9870 <swh9870@naver.com>
2020-12-16 23:03:47 +0900

    'aaaa'

---
+++ ch11/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"/>
+    <title>Nav bar</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/d7391bd90c.js" crossorigin="anonymous"></script>
+    <script src='main.js' defer></script>
+</head>
+<body>
+    <nav class="navber">
+        <div class="navber_logo">
+            <i class="fab fa-accusoft"></i>
+            <a href="">DreamCoding</a>
+        </div>
+        <ul class="navbar_menu">
+            <li><a href="">Home</a></li>
+            <li><a href="">Gallery</a></li>
+            <li><a href="">Weddings</a></li>
+            <li><a href="">FAQ</a></li>
+            <li><a href="">Bookings</a></li>
+            
+        </ul>
+        <ul class="navber_icons">
+            <li><i class="fab fa-twitter"></i></li>
+            <li><i class="fab fa-facebook"></i></li>
+        </ul>
+        <a href="#" class="navber_toogleBtn">
+            <i class="fas fa-bars"></i>
+        </a>
+    </nav>
+</body>
+</html>
